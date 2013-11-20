aaf80cabddb6f02868e7dc8368c85d95512339f7
Claudio Ortolina <claudio@new-bamboo.co.uk>
2013-11-20 07:29:18 +0000

    Updates to rivets 0.6.

--- app/scripts/config/rivets.js
+++ app/scripts/config/rivets.js
@@ -1,21 +1,20 @@
 define(['rivets', 'moment'], function (rivets, moment) {
   'use strict';
-  rivets.configure({
-    adapter: {
-      subscribe: function(obj, keypath, callback) {
-        obj.on('change:' + keypath, callback);
-      },
-      unsubscribe: function(obj, keypath, callback) {
-        obj.off('change:' + keypath, callback);
-      },
-      read: function(obj, keypath) {
-        return obj.get(keypath);
-      },
-      publish: function(obj, keypath, value) {
-        obj.set(keypath, value);
-      }
+
+  rivets.adapters[':'] = {
+    subscribe: function(obj, keypath, callback) {
+      obj.on('change:' + keypath, callback);
+    },
+    unsubscribe: function(obj, keypath, callback) {
+      obj.off('change:' + keypath, callback);
+    },
+    read: function(obj, keypath) {
+      return obj.get(keypath);
+    },
+    publish: function(obj, keypath, value) {
+      obj.set(keypath, value);
     }
-  });
+  };
 
   rivets.formatters.celsius = function (value) {
     var celsius = (value  -  32) * 5/9;
